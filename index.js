12b6184157268113f5f9fabb3beda22fe499b704
bguiz <bguiz@users.noreply.github.com>
2020-04-14 15:53:49 +0800

    step-02-03 - add main js file

---
+++ index.js
@@ -0,0 +1,55 @@
+// Source code to interact with smart contract
+
+//connection with node using web3 injected
+if (window.ethereum) {
+  window.web3 = new Web3(window.ethereum)
+  window.ethereum.enable()
+}
+else if (window.web3) {
+  window.web3 = new Web3(window.web3.currentProvider)
+}
+else {
+  window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+}
+console.log (window.web3.currentProvider)
+
+
+// contractAddress and abi are setted after contract deploy
+var contractAddress = '0xc864D0fef177A69aFa8E302A1b90e450910A4c3E';
+var abi = JSON.parse( '[{"constant":true,"inputs":[],"name":"getInfo","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_info","type":"string"}],"name":"setInfo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]' );
+
+//contract instance
+contract = new web3.eth.Contract(abi, contractAddress);
+
+// Accounts
+var account;
+
+web3.eth.getAccounts(function(err, accounts) {
+  if (err != null) {
+    alert("Error retrieving accounts.");
+    return;
+  }
+  if (accounts.length == 0) {
+    alert("No account found! Make sure the Ethereum client is configured properly.");
+    return;
+  }
+  account = accounts[0];
+  console.log('Account: ' + account);
+  web3.eth.defaultAccount = account;
+});
+
+//Smart contract functions
+function registerSetInfo() {
+  info = $("#newInfo").val();
+  contract.methods.setInfo (info).send( {from: account}).then( function(tx) {
+    console.log("Transaction: ", tx);
+  });
+  $("#newInfo").val('');
+}
+
+function registerGetInfo() {
+  contract.methods.getInfo().call().then( function( info ) {
+    console.log("info: ", info);
+    document.getElementById('lastInfo').innerHTML = info;
+  });
+}
