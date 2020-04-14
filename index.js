8b64d1fdfc557c22a67f78779a923103f0e9dcb6
bguiz <bguiz@users.noreply.github.com>
2020-04-14 15:54:54 +0800

    step-02-04 - update smart contract adddress to one just deployed

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ console.log (window.web3.currentProvider)
 
 
 // contractAddress and abi are setted after contract deploy
-var contractAddress = '0xc864D0fef177A69aFa8E302A1b90e450910A4c3E';
+var contractAddress = '0x2ad43002262aD9Ba3975CcB93B241FA1CcF11572';
 var abi = JSON.parse( '[{"constant":true,"inputs":[],"name":"getInfo","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_info","type":"string"}],"name":"setInfo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]' );
 
 //contract instance
